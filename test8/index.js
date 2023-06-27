b256d46f08ad73d60813fd90c5a3699461d391dd
Ngawanglm <109843746+ngawanglm07@users.noreply.github.com>
2023-06-27 21:00:52 +0530

    thisis

---
+++ test8/index.js
@@ -0,0 +1,21 @@
+const url = 'https://text-translator2.p.rapidapi.com/getLanguages';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5dfeb94f93mshb236fca09fa2e4ap1ec5b0jsnaedb9096adb9',
+		'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
+	}
+};
+fetch(url , options)
+.then((response)=> response.json())
+.then(data => {
+	console.log(data);
+  let randomNumber = Math.floor(Math.random() * 111)
+	const language =data.data.languages[randomNumber].name;
+
+	document.getElementById('output').innerHTML = `<p>${language}</p>`
+})
+.catch((error) => {
+	console.log(error);
+	alert('this is an error');
+})
