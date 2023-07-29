2c0fec8347e67e1cf6350d55439f70cd370ea49b
Ngawanglm <109843746+ngawanglm07@users.noreply.github.com>
2023-07-29 18:37:30 +0530

    thug lige

--- stack/duplicates.js
+++ stack/duplicates.js
@@ -1,7 +1,18 @@
 let arr = [1,2,3,4,5,6,7,1,1,2,2,3,2,3,2]
 
 
-let duplicates = arr.filter((ele, index , arr) => arr.indexOf(ele) !== index)
-console.log(duplicates);
+// let duplicates = arr.filter((ele, index , arr) => arr.indexOf(ele) !== index)
+// console.log(duplicates);
 
+let duplicates = [];
+let seenSet = new Set();
 
+arr.forEach((ele) =>{
+    if(seenSet.has(ele)){
+        duplicates.push(ele);
+    } else {
+        seenSet.add(ele)
+    }
+})
+
+console.log(duplicates)
